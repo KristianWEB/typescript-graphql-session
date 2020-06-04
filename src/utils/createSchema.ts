07c3970ea12e96e8971aa76b5f500a98b6a94926
Kristian <devWEBpk@outlook.com>
2020-06-04 15:26:59 +0200

    hoc resolvers, file uploads

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -6,6 +6,11 @@ import { LoginResolver } from "../modules/user/Login";
 import { LogoutResolver } from "../modules/user/Logout";
 import { MeResolver } from "../modules/user/Me";
 import { RegisterResolver } from "../modules/user/Register";
+import {
+  CreateUserResolver,
+  CreateProductResolver,
+} from "../modules/user/CreateUser";
+import { ProfilePictureResolver } from "../modules/user/ProfilePicture";
 
 export const createSchema = () =>
   buildSchema({
@@ -17,6 +22,9 @@ export const createSchema = () =>
       LogoutResolver,
       MeResolver,
       RegisterResolver,
+      CreateUserResolver,
+      CreateProductResolver,
+      ProfilePictureResolver,
     ],
     authChecker: ({ context: { req } }) => !!req.session.userId,
   });
