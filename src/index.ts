4132755d28e9a2af107f530d4764fca71e4c9d30
Kristian <devWEBpk@outlook.com>
2020-06-04 15:53:35 +0200

    query complexity

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { createConnection } from "typeorm";
-import cors from "cors";
 import session from "express-session";
 import connectRedis from "connect-redis";
+import cors from "cors";
+import {
+  getComplexity,
+  simpleEstimator,
+  fieldExtensionsEstimator,
+} from "graphql-query-complexity";
+
 import { redis } from "./redis";
 import { createSchema } from "./utils/createSchema";
 
@@ -15,7 +21,52 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema,
-    context: ({ req, res }) => ({ req, res }),
+    context: ({ req, res }: any) => ({ req, res }),
+    plugins: [
+      {
+        requestDidStart: () => ({
+          didResolveOperation({ request, document }) {
+            /**
+             * This provides GraphQL query analysis to be able to react on complex queries to your GraphQL server.
+             * This can be used to protect your GraphQL servers against resource exhaustion and DoS attacks.
+             * More documentation can be found at https://github.com/ivome/graphql-query-complexity.
+             */
+            const complexity = getComplexity({
+              // Our built schema
+              schema,
+              // To calculate query complexity properly,
+              // we have to check only the requested operation
+              // not the whole document that may contains multiple operations
+              operationName: request.operationName,
+              // The GraphQL query document
+              query: document,
+              // The variables for our GraphQL query
+              variables: request.variables,
+              // Add any number of estimators. The estimators are invoked in order, the first
+              // numeric value that is being returned by an estimator is used as the field complexity.
+              // If no estimator returns a value, an exception is raised.
+              estimators: [
+                // Using fieldExtensionsEstimator is mandatory to make it work with type-graphql.
+                fieldExtensionsEstimator(),
+                // Add more estimators here...
+                // This will assign each field a complexity of 1
+                // if no other estimator returned a value.
+                simpleEstimator({ defaultComplexity: 1 }),
+              ],
+            });
+            // Here we can react to the calculated complexity,
+            // like compare it with max and throw error when the threshold is reached.
+            if (complexity >= 8) {
+              throw new Error(
+                `Sorry, too complicated query! ${complexity} is over 8 that is the max allowed complexity.`
+              );
+            }
+            // And here we can e.g. subtract the complexity point from hourly API calls limit.
+            console.log("Used query complexity points:", complexity);
+          },
+        }),
+      },
+    ],
   });
 
   const app = Express();
@@ -31,22 +82,25 @@ const main = async () => {
 
   app.use(
     session({
-      store: new RedisStore({ client: redis }),
+      store: new RedisStore({
+        client: redis as any,
+      }),
       name: "qid",
-      secret: "asldfjaskdjf",
+      secret: "aslkdfjoiq12312",
       resave: false,
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        maxAge: 1000 * 60 * 60 * 25 * 7 * 365, // 7 years
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
       },
     })
   );
 
   apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
-    console.log("server started on http://localhost:4000");
+    console.log("server started on http://localhost:4000/graphql");
   });
 };
 
