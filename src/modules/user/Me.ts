d73b3e4cbdb12d005ab25598744de547fab3c7d7
Kristian <devWEBpk@outlook.com>
2020-06-16 17:28:53 +0300

    change protected queries message

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -7,7 +7,7 @@ export class MeResolver {
   @Query(() => User, { nullable: true, complexity: 5 })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     if (!ctx.req.session!.userId) {
-      return undefined;
+      throw new Error("Not authenticated!");
     }
     return User.findOne(ctx.req.session!.userId);
   }
